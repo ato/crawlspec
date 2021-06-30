97b3d741891c0382c328b2e5aa0d6abd6bfc2ba6
Alex Osborne <aosborne@nla.gov.au>
2021-06-30 11:33:23 +0900

    heritrix: generate seeds.txt and surts.txt

--- src/org/netpreserve/crawlspec/crawler/Heritrix.java
+++ src/org/netpreserve/crawlspec/crawler/Heritrix.java
@@ -4,16 +4,23 @@ import org.netpreserve.crawlspec.ConfigWriter;
 import org.netpreserve.crawlspec.Crawler;
 import org.netpreserve.crawlspec.job.Job;
 import org.netpreserve.crawlspec.job.RobotsPolicy;
+import org.netpreserve.crawlspec.job.Scope;
+import org.netpreserve.crawlspec.job.Seed;
 import org.netpreserve.crawlspec.util.Command;
 import org.netpreserve.crawlspec.util.Properties;
+import org.netpreserve.urlcanon.Canonicalizer;
+import org.netpreserve.urlcanon.ParsedUrl;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class Heritrix implements Crawler {
     @Override
     public void writeConfig(Job job, ConfigWriter configWriter) throws IOException {
         configWriter.writeRunCommand(buildCommand(job));
-        configWriter.writeFile("override.properties", buildProperties(job).serialize());
+        configWriter.writeFile("overrides.properties", buildOverrides(job).serialize());
+        configWriter.writeFile("surts.txt", buildSurtsTxt(job));
+        configWriter.writeFile("seeds.txt", buildSeedsTxt(job));
         if (job.hasCookies()) {
             configWriter.writeCookiesTxt(job.getCookies());
         }
@@ -23,10 +30,11 @@ public class Heritrix implements Crawler {
     public Command buildCommand(Job job) {
         Command command = new Command();
         command.add("heritrix");
+        command.addOption("-r", job.getId());
         return command;
     }
 
-    public Properties buildProperties(Job job) {
+    public Properties buildOverrides(Job job) {
         Properties properties = new Properties();
         properties.put("metadata.jobName", job.getId());
         properties.put("metadata.userAgentTemplate", job.getUserAgent());
@@ -40,5 +48,68 @@ public class Heritrix implements Crawler {
             properties.put("cookieStore.cookiesLoadFile", "cookies.txt");
         }
         return properties;
+    }
+
+    public ParsedUrl parseAndCanonicalize(String url) {
+        ParsedUrl parsedUrl = ParsedUrl.parseUrl(url);
+
+        // default to http:// if there's no scheme
+        if (parsedUrl.getScheme().isEmpty()) {
+            parsedUrl = ParsedUrl.parseUrl("http://" + url);
+        }
+
+        Canonicalizer.WHATWG.canonicalize(parsedUrl);
+        return parsedUrl;
+    }
+
+    private Stream<String> buildSeedsTxt(Job job) {
+        return job.getSeeds().stream().map(Seed::getUrl);
+    }
+
+    private Stream<String> buildSurtsTxt(Job job) {
+        return job.getSeeds().stream().map(this::buildSurt);
+    }
+
+    public String buildSurt(Seed seed) {
+        return buildSurt(seed.getUrl(), seed.getScope());
+    }
+
+    public String buildSurt(String url, Scope scope) {
+        ParsedUrl parsedUrl = parseAndCanonicalize(url);
+        if (scope == null) {
+            scope = parsedUrl.getPath().equals("/") ? Scope.DOMAIN : Scope.DIRECTORY;
+        }
+        switch (scope) {
+            case PAGE:
+                return parsedUrl.surt();
+            case DIRECTORY:
+                parsedUrl.setFragment("");
+                parsedUrl.setHashSign("");
+                parsedUrl.setQuery("");
+                parsedUrl.setQuestionMark("");
+                parsedUrl.setPath(parsedUrl.getPath().replaceFirst("[^/]+$", ""));
+                return parsedUrl.surt();
+            case HOST:
+                parsedUrl.setFragment("");
+                parsedUrl.setHashSign("");
+                parsedUrl.setQuery("");
+                parsedUrl.setQuestionMark("");
+                parsedUrl.setPath("/");
+                return parsedUrl.surt();
+            case DOMAIN:
+                parsedUrl.setFragment("");
+                parsedUrl.setHashSign("");
+                parsedUrl.setQuery("");
+                parsedUrl.setQuestionMark("");
+                parsedUrl.setPath("");
+                parsedUrl.setPort("");
+                parsedUrl.setColonBeforePort("");
+                if (parsedUrl.getHost().startsWith("www.")) {
+                    parsedUrl.setHost(parsedUrl.getHost().substring("www.".length()));
+                }
+                return parsedUrl.surt().replaceFirst("\\)$", "");
+            default:
+                throw new IllegalArgumentException("unimplemented scope: " + scope);
+        }
     }
 }
--- test/org/netpreserve/crawlspec/crawler/HeritrixCrawlerTest.java
+++ test/org/netpreserve/crawlspec/crawler/HeritrixCrawlerTest.java
@@ -2,6 +2,7 @@ package org.netpreserve.crawlspec.crawler;
 
 import org.junit.jupiter.api.Test;
 import org.netpreserve.crawlspec.job.JobTest;
+import org.netpreserve.crawlspec.job.Scope;
 
 import java.util.Map;
 import java.util.TreeMap;
@@ -9,7 +10,6 @@ import java.util.TreeMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HeritrixCrawlerTest {
-
     @Test
     void buildProperties() {
         Map<String,String> expected = new TreeMap<>();
@@ -18,6 +18,18 @@ class HeritrixCrawlerTest {
         expected.put("metadata.jobName", "example-job");
         expected.put("metadata.userAgentTemplate", "crawlspec_bot/2.0");
         expected.put("cookieStore.cookiesLoadFile", "cookies.txt");
-        assertEquals(expected, new Heritrix().buildProperties(JobTest.testJob).map());
+        assertEquals(expected, new Heritrix().buildOverrides(JobTest.testJob).map());
+    }
+
+    @Test
+    public void buildSurt() {
+        Heritrix heritrix = new Heritrix();
+        assertEquals("http://(com,example,www,)/dir/index.html", heritrix.buildSurt("http://www.example.com/dir/index.html", Scope.PAGE));
+        assertEquals("http://(com,example,www,)/dir/", heritrix.buildSurt("http://www.example.com/dir/index.html?q#f", Scope.DIRECTORY));
+        assertEquals("http://(com,example,www,:81)/", heritrix.buildSurt("http://www.example.com:81/dir/index.html?q#f", Scope.HOST));
+        assertEquals("http://(com,example,", heritrix.buildSurt("http://www.example.com:81/dir/index.html?q#f", Scope.DOMAIN));
+        assertEquals("http://(com,example,www,)/", heritrix.buildSurt("http://www.example.com", Scope.PAGE));
+        assertEquals("http://(com,example,", heritrix.buildSurt("http://www.example.com", null));
+        assertEquals("http://(com,example,www,)/", heritrix.buildSurt("http://www.example.com/foo.html", null));
     }
 }
